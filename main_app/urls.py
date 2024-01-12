88e0e3cb0ae8ccfbe06fb050145f88922d004538
Ryan Luis <RyanCLuis@gmail.com>
2024-01-12 09:00:05 -0800

    home route is started

---
+++ main_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+# this file is how we map our urls to views
+# views are like our controllers, they map HTTP requests to code. This file is how we match those requests to specific urls.
+# the line 'name='home'' is a kwarg, which gives the route a name. naming routes is optional, but best practices.
+# later in the codealong, we'll see just how useful it is to name out routes
+urlpatterns = [
+    # first arg - url endpoint
+    # sec arg - the view to render
+    # third arg(opt) - names the route 
+	path('', views.home, name = 'home')
+]
---
+++ main_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# define home view here - '/'
+# Ger - Home
+def home(request):
+    # unlike with ejs, we need our html file extension
+    return render(request, 'home.html')
